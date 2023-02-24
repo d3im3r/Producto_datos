a3567f993703c133993c8b3dab3ed5362fed9509
Juan Bula <jdbula11@gmail.com>
2023-02-23 23:54:39 -0500

    Depure dependences

--- Flujo/simulator.py
+++ Flujo/simulator.py
@@ -3,17 +3,7 @@ import os
 from datetime import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import geopandas
-import folium
-import plotly.io as pli
 from sklearn.preprocessing import MinMaxScaler
-
-# Don´t show warnings......
-import warnings
-
-warnings.filterwarnings("ignore")
 pd.set_option('display.max_columns', 10)
 
 
