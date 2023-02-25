28374fe302709805e40118c17da492f7352e7618
saagudelore <saagudelore@unal.edu.co>
2023-02-24 22:48:53 -0500

    Actualziación flujo

--- Flujo/simulator.py
+++ Flujo/simulator.py
@@ -4,16 +4,23 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-pd.set_option('display.max_columns', 10)
+# pd.set_option('display.max_columns', 10)
 
 
 #####################################################################################################################################
-
+path_ = os.path.dirname(__file__)
+categories_unusefull = ["agent","company",'days_in_waiting_list', 'arrival_date_year', "arrival_date_month",'assigned_room_type', 'booking_changes',
+                        'reservation_status', 'country', 'days_in_waiting_list']
 
 def main_work():
     data_raw = load_data()
     data_selected = select_data(data_raw)
-    data_4_use = define_status(data_selected,flag=21)
+    data_clean = data_clean_columns(data_selected,categories_unusefull)
+    data_datetime_corrected = datetime_adjust(data_clean)
+    data_status = define_status(data_datetime_corrected)
+    data_OHE = one_hot_encoding(data_status)
+    data_4_use = data_scaling(data_OHE)
+    data_4_use.to_csv(f"../Data/Preprocessing/preprocessing_data_{1}.csv", index = False, header = True)
     return data_4_use
 
 
@@ -35,24 +42,15 @@ def select_data(booking,instances=10000,seed=42):
     return booking
 
 ## limpieza de datos
-def data_clean_columns(booking,head1=None,head2=None,head3=None,head4=None,head5=None):
+def data_clean_columns(booking,heads_to_remove=[]):
     # Eliminando variables que no aportaran al modelo...
-    if head1 in booking[0:]:
-        booking.drop(head1, axis=1, inplace=True)
-    if head2 in booking[0:]:
-        booking.drop(head2, axis=1, inplace=True)
-    if head3 in booking[0:]:
-        booking.drop(head3, axis=1, inplace=True)
-    if head4 in booking[0:]:
-        booking.drop(head4, axis=1, inplace=True)
-    if head5 in booking[0:]:
-        booking.drop(head5, axis=1, inplace=True)
-    booking = booking.dropna()
+    for head in heads_to_remove:
+        if head in booking[0:]:
+            booking.drop(head, axis=1, inplace=True)
     print("#"*50,"\nDatos nulos presentes después de la limpieza",booking.isnull().sum())
     return booking
 
-
-def define_status(booking,flag=7):
+def define_status(booking,flag=21):
     data = booking.apply(lambda row: categorize_status(row,flag), axis=1)
     booking.insert(loc=3,
                    column='status',
@@ -60,7 +58,7 @@ def define_status(booking,flag=7):
     
     return booking
 
-def categorize_status(row,flag=7):
+def categorize_status(row,flag=21):
     if row['lead_time'] == 0:
         return 'closed'
     elif row['lead_time'] > 0 and row['lead_time'] <= flag:
@@ -69,9 +67,36 @@ def categorize_status(row,flag=7):
         return 'open'
 
 def data_scale(booking):
+    x=0
+
+def datetime_adjust(booking):
+    booking['reservation_status_date'] = pd.to_datetime(booking['reservation_status_date'])
+    booking['year_res_status_date'] = booking['reservation_status_date'].dt.year
+    booking['month_res_status_date'] = booking['reservation_status_date'].dt.month
+    booking['day_res_status_date'] = booking['reservation_status_date'].dt.day
+    # Eliminando reservation status date y arrival month...
+    booking.drop(['reservation_status_date'] , axis = 1, inplace = True)
+    return booking
+
+def one_hot_encoding(booking):
+    return pd.get_dummies(booking)
+
+def data_scaling(booking):
+    # Escalando...
+    scaler = MinMaxScaler()
+    booking_processing_scaled = scaler.fit_transform(booking)
+    # Convertir a tipo DataFrame...
+    data_processing_scaled = pd.DataFrame(booking_processing_scaled)
+    # Obteniendo nombres columnas...
+    data_processing_scaled.columns = booking.columns
+    data_processing_scaled.head()
+    print(data_processing_scaled.shape)
+    return data_processing_scaled
 
 
-datos=main_work()
-datos=data_clean_columns(datos,"agent","company","country")
+##########################
+
+
+datos = main_work()
 
 print(datos.head())
