49920a38147e56986243e61b6e779499e11957f8
Juan Bula <jdbula11@gmail.com>
2023-02-25 00:18:45 -0500

    Flujo V1

--- Flujo/simulator.py
+++ Flujo/simulator.py
@@ -14,12 +14,20 @@ categories_unusefull = ["agent","company",'days_in_waiting_list', 'arrival_date_
 
 def main_work():
     data_raw = load_data()
+    print(" Dimensionalidad datos originales... ".center(80,"*"))
+    print(data_raw.shape)
     data_selected = select_data(data_raw)
+    print(" Muestreando data... ".center(80,"*"), data_selected.shape, data_selected.head(), sep="\n")
     data_clean = data_clean_columns(data_selected,categories_unusefull)
+    print(" Limpiando data... ".center(80,"*"),data_clean.head(), sep="\n")
     data_datetime_corrected = datetime_adjust(data_clean)
+    print(" Definiendo variable status... ".center(80,"*"))
     data_status = define_status(data_datetime_corrected)
-    data_OHE = one_hot_encoding(data_status)
+    data_feeder = data_filter(data_status)
+    data_OHE = one_hot_encoding(data_feeder)
+    print(" One-Hot-Encoding... ".center(80,"*"), data_OHE.shape, sep="\n")
     data_4_use = data_scaling(data_OHE)
+    print(" Escalando data... ".center(80,"*"), data_4_use.shape,data_4_use.head(),sep="\n")
     data_4_use.to_csv(f"../Data/Preprocessing/preprocessing_data_{1}.csv", index = False, header = True)
     return data_4_use
 
@@ -36,9 +44,9 @@ def load_data():
 ## muestreo aleatorio simple
 def select_data(booking,instances=10000,seed=42):
     booking = booking.sample(n=instances, random_state=seed)
-    print("#"*50,"\nDimension del dataframe: ",booking.shape)
-    print("#"*50,"\nNumero de registros por hotel: \n",booking.hotel.value_counts())
-    print("#"*50,"\nDatos nulos presentes",booking.isnull().sum())
+    #print("#"*50,"\nDimension del dataframe: ",booking.shape)
+    #print("#"*50,"\nNumero de registros por hotel: \n",booking.hotel.value_counts())
+    #print("#"*50,"\nDatos nulos presentes",booking.isnull().sum())
     return booking
 
 ## limpieza de datos
@@ -47,8 +55,10 @@ def data_clean_columns(booking,heads_to_remove=[]):
     for head in heads_to_remove:
         if head in booking[0:]:
             booking.drop(head, axis=1, inplace=True)
-    print("#"*50,"\nDatos nulos presentes después de la limpieza",booking.isnull().sum())
+    #print("#"*50,"\nDatos nulos presentes después de la limpieza",booking.isnull().sum())
     return booking
+
+
 
 def define_status(booking,flag=21):
     data = booking.apply(lambda row: categorize_status(row,flag), axis=1)
@@ -66,8 +76,10 @@ def categorize_status(row,flag=21):
     else:
         return 'open'
 
-def data_scale(booking):
-    x=0
+def data_filter(booking):
+    booking = booking[booking["lead_time"]<=21]
+    return booking
+
 
 def datetime_adjust(booking):
     booking['reservation_status_date'] = pd.to_datetime(booking['reservation_status_date'])
@@ -90,13 +102,8 @@ def data_scaling(booking):
     # Obteniendo nombres columnas...
     data_processing_scaled.columns = booking.columns
     data_processing_scaled.head()
-    print(data_processing_scaled.shape)
+    #print(data_processing_scaled.shape)
     return data_processing_scaled
 
 
 ##########################
-
-
-datos = main_work()
-
-print(datos.head())
