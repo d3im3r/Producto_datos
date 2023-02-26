f7c126dc27b92d9dc372eabe5235d74e0eaa31cc
saagudelore <saagudelore@unal.edu.co>
2023-02-26 16:43:17 -0500

    Orden codigo

--- Flujo/simulator.py
+++ Flujo/simulator.py
@@ -8,9 +8,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 #####################################################################################################################################
-path_ = os.path.dirname(__file__)
 categories_unusefull = ["agent","company",'days_in_waiting_list', 'arrival_date_year', "arrival_date_month",'assigned_room_type', 'booking_changes',
                         'country', 'days_in_waiting_list']
+data_path = "hotel_bookings.csv"
+flag = 21 # Cantidad de dias con los que se van a analizar
 
 def main_work():
     data_raw = load_data()
@@ -22,9 +23,9 @@ def main_work():
     print(" Limpiando data... ".center(80,"*"),data_clean.head(), sep="\n")
     data_datetime_corrected = datetime_adjust(data_clean)
     print(" Definiendo variable status... ".center(80,"*"))
-    data_status = define_status(data_datetime_corrected)
+    data_status = define_status(data_datetime_corrected,flag)
     print(data_status.loc[:, ['reservation_status', "is_canceled"]])
-    data_feeder = data_filter(data_status)
+    data_feeder = data_filter(data_status,flag)
     data_OHE = one_hot_encoding(data_feeder)
     print(" One-Hot-Encoding... ".center(80,"*"), data_OHE.shape, sep="\n")
     data_4_use = data_scaling(data_OHE)
@@ -32,93 +33,93 @@ def main_work():
     data_4_use.to_csv(f"../Data/Preprocessing/preprocessing_data_{1}.csv", index = False, header = True)
     return data_4_use
 
-
-## carga de datos
+'''
+Carga de informacion
+Lectura de archivo csv en la ruta data_path
+'''
 def load_data():
     path_ = os.path.dirname(__file__)
-    filename = os.path.join(path_, "hotel_bookings.csv")
+    filename = os.path.join(path_, data_path)
     if not os.path.exists(filename):
         raise FileNotFoundError(f"File {filename} not found")
-    booking = pd.read_csv(filename, sep=",")
-    return booking
+    dataframe = pd.read_csv(filename, sep=",")
+    return dataframe
 
-## muestreo aleatorio simple
-def select_data(booking,instances=10000,seed=42):
-    booking = booking.sample(n=instances, random_state=seed)
-    #print("#"*50,"\nDimension del dataframe: ",booking.shape)
-    #print("#"*50,"\nNumero de registros por hotel: \n",booking.hotel.value_counts())
-    #print("#"*50,"\nDatos nulos presentes",booking.isnull().sum())
-    return booking
+'''
+Muestreo aleatorio simple
+'''
+def select_data(dataframe,instances=10000,seed=42):
+    dataframe = dataframe.sample(n=instances, random_state=seed)
+    return dataframe
 
-## limpieza de datos
-def data_clean_columns(booking,heads_to_remove=[]):
-    # Eliminando variables que no aportaran al modelo...
+'''
+Se limpian datos de columnas poco relevantes para el 
+modelo final
+'''
+def data_clean_columns(dataframe,heads_to_remove=[]):
     for head in heads_to_remove:
-        if head in booking[0:]:
-            booking.drop(head, axis=1, inplace=True)
-    #print("#"*50,"\nDatos nulos presentes después de la limpieza",booking.isnull().sum())
-    return booking
+        if head in dataframe[0:]:
+            dataframe.drop(head, axis=1, inplace=True)
+    return dataframe
 
+'''
+Se cambia el estado de reserva en base a si fue cancelada 
+o no en el tiempo que hay entre la reserva y el dia 
+de llegada al hotel
+'''
+def define_status(dataframe,flag=21):
+    dataframe["reservation_status"] = dataframe.apply(lambda row: categorize_status(row,flag), axis=1)
+    return dataframe
 
-def define_status(booking,flag=21):
-    booking["reservation_status"] = booking.apply(lambda row: categorize_status(row,flag), axis=1)
-    return booking
-
-def categorize_status(booking,flag=21):
-    if booking['is_canceled'] == 1:
+def categorize_status(dataframe,flag=21):
+    if dataframe['is_canceled'] == 1:
         result = "canceled"
     else:
-        if booking['lead_time'] == 0:
+        if dataframe['lead_time'] == 0:
             result = "closed"
-        elif booking['lead_time'] > 0 and booking['lead_time'] <= flag:
+        elif dataframe['lead_time'] > 0 and dataframe['lead_time'] <= flag:
             result = "in_progress"
         else:
             result = "open"
     return result
 
-def define_status_2(booking,flag=21):
-    data = booking.apply(lambda row: categorize_status_2(row,flag), axis=1)
-    booking.insert(loc=3,
-                   column='status',
-                   value = data)
-    
-    return booking
+'''
+Se seleccionan los datos que se encuentren dentro de 
+'''
+def data_filter(dataframe,flag=21):
+    dataframe = dataframe[dataframe["lead_time"]<=flag]
+    return dataframe
 
-def categorize_status_2(row,flag=21):
-    if row['lead_time'] == 0:
-        return 'closed'
-    elif row['lead_time'] > 0 and row['lead_time'] <= flag:
-        return 'in_progress'
-    else:
-        return 'open'
-
-def data_filter(booking):
-    booking = booking[booking["lead_time"]<=21]
-    return booking
-
-
-def datetime_adjust(booking):
-    booking['reservation_status_date'] = pd.to_datetime(booking['reservation_status_date'])
-    booking['year_res_status_date'] = booking['reservation_status_date'].dt.year
-    booking['month_res_status_date'] = booking['reservation_status_date'].dt.month
-    booking['day_res_status_date'] = booking['reservation_status_date'].dt.day
+'''
+Se convierte la fecha de reservation status a año, mes y dia
+'''
+def datetime_adjust(dataframe):
+    dataframe['reservation_status_date'] = pd.to_datetime(dataframe['reservation_status_date'])
+    dataframe['year_res_status_date'] = dataframe['reservation_status_date'].dt.year
+    dataframe['month_res_status_date'] = dataframe['reservation_status_date'].dt.month
+    dataframe['day_res_status_date'] = dataframe['reservation_status_date'].dt.day
     # Eliminando reservation status date y arrival month...
-    booking.drop(['reservation_status_date'] , axis = 1, inplace = True)
-    return booking
+    dataframe.drop(['reservation_status_date'] , axis = 1, inplace = True)
+    return dataframe
 
-def one_hot_encoding(booking):
-    return pd.get_dummies(booking)
+'''
+Se convierten todos los valores a numeros entre 0 y 1
+'''
+def one_hot_encoding(dataframe):
+    return pd.get_dummies(dataframe)
 
-def data_scaling(booking):
+'''
+Se escalan los datos mediante fit_transform
+'''
+def data_scaling(dataframe):
     # Escalando...
     scaler = MinMaxScaler()
-    booking_processing_scaled = scaler.fit_transform(booking)
+    dataframe_processing_scaled = scaler.fit_transform(dataframe)
     # Convertir a tipo DataFrame...
-    data_processing_scaled = pd.DataFrame(booking_processing_scaled)
+    data_processing_scaled = pd.DataFrame(dataframe_processing_scaled)
     # Obteniendo nombres columnas...
-    data_processing_scaled.columns = booking.columns
+    data_processing_scaled.columns = dataframe.columns
     data_processing_scaled.head()
-    #print(data_processing_scaled.shape)
     return data_processing_scaled
 
 
