bc250ca2079c25ba3f9e5685b09f4ab2e5bbde43
d3im3r <demiranda@unal.edu.co>
2023-02-27 17:51:19 -0500

    Simulation and process. fix path error. Make done!

--- Flujo/process.py
+++ Flujo/process.py
@@ -2,9 +2,9 @@ import sys
 import test
 
 if __name__ == '__main__':
-    if len(sys.argv) == 0:
-        test.main_work(1)
+    if len(sys.argv) == 1:
+        test.process_work(1)
     else:
         n_days=int(sys.argv[1])
-        test.main_work(n_days)
+        test.process_work(n_days)
 
