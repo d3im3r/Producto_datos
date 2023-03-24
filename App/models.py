4f22215bcd2139dbedd676e7e11cfc9882c95052
saagudelore <saagudelore@unal.edu.co>
2023-03-23 21:41:53 -0500

    Se quitan valores quemados en la pestaña de Métricas

--- App/models.py
+++ App/models.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import ConfusionMatrixDisplay, classification_report, accuracy_score
 
 from utils import Utils
 
@@ -30,20 +31,27 @@ class Models:
         }
     
     # Definir funcion de ejecucion codigo...
-    def grid_training(self,X,y):
+    def grid_training(self,x_train,y_train,x_test,y_test):
 
         best_score = 999
         best_model = None
 
         for name, reg in self.reg.items():
 
-            grid_reg = RandomizedSearchCV(reg,self.params[name],cv=3).fit(X,y.values.ravel())
+            grid_reg = RandomizedSearchCV(reg,self.params[name],cv=3).fit(x_train,y_train.values.ravel())
             score = np.abs(grid_reg.best_score_)
 
             if score < best_score:
                 best_score = score
                 best_model = grid_reg.best_estimator_
 
+            gbt_preds_opt = grid_reg.predict(x_test)
+            accuracy = accuracy_score(y_test,gbt_preds_opt)
+
+            target_names = ['Non-Canceled', 'Canceled']
+            classification_repo = classification_report(y_test, gbt_preds_opt, target_names=target_names)
+
 
         utils = Utils()
         utils.model_export(best_model,best_score)
+        return classification_repo, accuracy
